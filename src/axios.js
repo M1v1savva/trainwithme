e74f736b6bbac5250ca4297cc9b2fe8037a46577
M1v1savva <ivan.polyakov.01@gmail.com>
2023-01-24 22:26:45 +0100

    upd

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'https://trainwithme-flask.onrender.com',
+    baseURL: process.env.REACT_APP_PROD_BASE_URL,
 });
 
 export default instance;
